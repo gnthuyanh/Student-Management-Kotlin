0865eab4d9257bed414b0c95c313e3410d882ebd
Thuy Anh <gnthuyanh@gmail.com>
2024-12-02 20:15:15 +0700

    update

---
+++ app/src/main/java/com/example/studentmanagement/database/StudentData.kt
@@ -0,0 +1,9 @@
+package com.example.studentmanagement.database
+
+import com.example.studentmanagement.model.Student
+object StudentData {
+    val students = mutableListOf(
+        Student("Nguyễn Văn A", "20210001"),
+        Student("Trần Thị B", "20210002"),
+    )
+}
